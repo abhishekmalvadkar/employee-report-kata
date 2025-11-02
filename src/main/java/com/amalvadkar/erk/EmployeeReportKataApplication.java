7f8a73993fd392f445eaca62bd6227cf1eb73057
Abhishek <abhi@dev.com>
2025-11-02 20:22:54 +0530

    Initial commit

---
+++ src/main/java/com/amalvadkar/erk/EmployeeReportKataApplication.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.erk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeReportKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeReportKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amalvadkar/erk/EmployeeReportKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.erk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeReportKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
